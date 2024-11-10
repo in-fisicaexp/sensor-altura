ab4a73ce8f7f396edffc1a5531db0ecda95e3c67
PabloCruzval <p.cruzat.valenzuela@gmail.com>
2024-11-10 17:55:35 -0300

    Change variables names and add some comments

--- CODIGOALTURA.ino
+++ CODIGOALTURA.ino
@@ -1,13 +1,16 @@
 #include <LiquidCrystal_I2C.h>
 
+// Sensor Pins
 int TRIG = 9;
 int ECO = 10;
-float DURACION;
-float DISTANCIA;
-float di;
+// Button Pin
+int button = 8;
+
+float duracion;
+float distancia;
+float altura = 200;
 float df;
 LiquidCrystal_I2C lcd(0x27,16,2);
-int button = 8;
 int pulsaciones = 0;
 bool firstPress = true;
 int totaldistancia=0;
@@ -29,41 +32,36 @@ void loop(){
   if(digitalRead(button) == LOW)
   {
     delay(700);
-    if(firstPress){
-
-      di = 200;
-      firstPress = false;
-    } else {
-      pulsaciones++;
-      if (pulsaciones % 1 == 0){
-        df = di - DISTANCIA;
-        totaldistancia += df;
-        Serial.println("Altura:");
-        Serial.print(df);
+    pulsaciones++;
+    if (pulsaciones % 1 == 0){
+      df = altura - distancia;
+      totaldistancia += df;
+      Serial.println("Altura:");
+      Serial.print(df);
+      Serial.println("cm");
+      Serial.println("Pulsaciones: ");
+      Serial.print(pulsaciones);
+      Serial.println();
+      Serial.println("Promedio: ");
+      if (135<(totaldistancia/(pulsaciones/1))<200)
+      {
+        Serial.print(totaldistancia/(pulsaciones/1));
         Serial.println("cm");
-        Serial.println("Pulsaciones: ");
-        Serial.print(pulsaciones);
-        Serial.println();
-        Serial.println("Promedio: ");
-        if (135<(totaldistancia/(pulsaciones/1))<200){
-          Serial.print(totaldistancia/(pulsaciones/1));
-          Serial.println("cm");
-        }
-        lcd.setCursor(0,0);
-        lcd.print("Altura: ");
-        lcd.print(df);
-        lcd.print("cm");
-        lcd.setCursor(0,1);
-        lcd.print("P:");
-        lcd.print(pulsaciones/1);
-        lcd.setCursor(5,1);
       }
-     }
-     digitalWrite(TRIG, HIGH);
-     delay(1);
-     digitalWrite(TRIG,LOW);
-     DURACION = pulseIn(ECO, HIGH);
-     DISTANCIA = (DURACION/58.2);
+      lcd.setCursor(0,0);
+      lcd.print("Altura: ");
+      lcd.print(df);
+      lcd.print("cm");
+      lcd.setCursor(0,1);
+      lcd.print("P:");
+      lcd.print(pulsaciones/1);
+      lcd.setCursor(5,1);
+    }
   }
+  digitalWrite(TRIG, HIGH);
+  delay(1);
+  digitalWrite(TRIG,LOW);
+  duracion = pulseIn(ECO, HIGH);
+  distancia = (duracion/58.2);
 }
 // by vstian
